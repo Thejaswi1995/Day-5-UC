648d26d1301f0abcb01d3edc7dd60afc2ec6210a
Thejaswi <thejaswirl187@gmail.com>
2022-03-02 18:27:24 +0530

    Day-5 UC

---
+++ Switch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+FRUIT="Kiwi"
+
+case "$FRUIT" in
+                 "apple")
+      echo "apple is red"
+;;
+"mango")
+echo "mango is juicy"
+;;
+"Kiwi")
+echo "Kiwi is sour"
+;;
+
